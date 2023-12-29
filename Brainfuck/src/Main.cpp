8164ef2206e1938033c77cdd722875e24a1c3b65
C42xe2 <erikdonath2006@gmail.com>
2023-12-29 13:52:27 +0100

    Reading input file
    Reading input file

--- Brainfuck/src/Main.cpp
+++ Brainfuck/src/Main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
 
 #include <cstdint>
 typedef int32_t  i32;
 typedef uint32_t u32;
 
 i32 main(i32 argc, char* argv[]) {
-	std::cout << "Brainfuck" << std::endl;
+	if (argc == 1) {
+		std::cout << "There is no input file given." << std::endl;
+		return 1;
+	}
+
+	std::fstream file(argv[1]);
+	if (!file.is_open()) {
+		std::cout << "The given input file does not exsist or can't be ready by this program." << std::endl;
+		return 1;
+	}
+
+	char c = '\0';
+	while (file.get(c)) {
+		std::cout << c;
+	}
+
+	file.close();
 }
